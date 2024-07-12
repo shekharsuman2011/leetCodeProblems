8e7096ac49ac3f0972434037cf77fdfb39a4e54f
shekharsuman1998@gmail.com <shekharsuman1998@gmail.com>
2024-07-12 18:21:59 +0530

    adding the testcase

--- problemsSolution/problemNo1190.java
+++ problemsSolution/problemNo1190.java
@@ -2,6 +2,11 @@ package problemsSolution;
 
 import java.util.Stack;
 
+//test cases
+//        System.out.println(reverseParentheses("(abcd)")); // Output: "dcba"
+//        System.out.println(reverseParentheses("(u(love)i)")); // Output: "iloveu"
+//        System.out.println(reverseParentheses("(ed(et(oc))el)")); // Output: "leetcode"
+
 public class problemNo1190 {
     public  static String reverseParentheses(String s) {
         Stack<Character> stack = new Stack<>();
